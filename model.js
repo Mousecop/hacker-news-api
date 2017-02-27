9f3f67dbf9efaaf8fa8900dc1f99f7bfd1485037
Mousecop <joaskew13@gmail.com>
2017-02-27 11:34:47 -0500

    make model schema

---
+++ model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const hackerNewsSchema = mongoose.Schema({
+    title: String,
+    url: String,
+    votes: {type: Number, default: 0}
+});
+
+hackerNewsSchema.methods.apiRepr = function() {
+    return {
+        id: this._id,
+        title: this.title,
+        url: this.url,
+        votes: this.votes
+    };
+}
+
+const HackerNews = mongoose.model('HackerNews', hackerNewsSchema);
+
+module.exports = {HackerNews};
